152749cfd038404a3dc34dee798d5178e81fea81
JoyceNguyen <jnguyen0581@gmail.com>
2019-02-20 02:37:35 -0600

    npm run build

---
+++ client/build/precache-manifest.d8a9c5f798e06aef6852634c36f3ca4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7b924f1b98af2089c343",
+    "url": "/static/js/main.7b924f1b.chunk.js"
+  },
+  {
+    "revision": "e20a7fb16d385ecadcdc",
+    "url": "/static/js/2.e20a7fb1.chunk.js"
+  },
+  {
+    "revision": "7b924f1b98af2089c343",
+    "url": "/static/css/main.413b0743.chunk.css"
+  },
+  {
+    "revision": "809e1637905e8abbbe07a74e8b94ba41",
+    "url": "/index.html"
+  }
+];
