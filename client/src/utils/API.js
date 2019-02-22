627cbca10938b634078b15369f8d20d3d015410b
JoyceNguyen <jnguyen0581@gmail.com>
2019-02-21 23:24:08 -0600

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,37 +1,49 @@
 import axios from 'axios';
 
-// const DEFAULT_HPP = '15';
+const DEFAULT_HPP = '15';
 
-// const PATH_BASE = 'https://www.googleapis.com/books/v1';
-// const PATH_SEARCH = '/volumes';
-// const PARAM_SEARCH = 'q=';
-// const PARAM_HPP = 'maxResults=';
-// const API_KEY = process.env.API_KEY;
-
-// export default {
-//   getGoogleBooks: function (search) {
-//     return axios.get(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${search}&${PARAM_HPP}${DEFAULT_HPP}&key=${API_KEY}`)
-//   },
+const PATH_BASE = 'https://www.googleapis.com/books/v1';
+const PATH_SEARCH = '/volumes';
+const PARAM_SEARCH = 'q=';
+const PARAM_HPP = 'maxResults=';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default {
-  // Get book from google search 
-  getGoogleBooks: function(query) {
-      return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+  getGoogleBooks: function (search) {
+    return axios.get(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${search}&${PARAM_HPP}${DEFAULT_HPP}&key=${API_KEY}`)
   },
-
+  
   saveBook: function (bookData) {
     return axios.post('/api/bookshelf', bookData);
   },
 
-  getAllBookshelf: function () {
+  getBookshelf: function () {
     return axios.get('/api/bookshelf')
   },
 
-  getBookshelf: function (id) {
-    return axios.get('/api/bookshelf' + id);
-  },
-
   deleteBook: function (id) {
-    return axios.delete('/api/bookshelf/' +id);
+    return axios.delete(`/api/bookshelf/${id}`)
   }
-}
+};
+// export default {
+//   // Get book from google search 
+//   getGoogleBooks: function(query) {
+//       return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+//   },
+
+//   saveBook: function (bookData) {
+//     return axios.post('/api/bookshelf', bookData);
+//   },
+
+//   // getAllBookshelf: function () {
+//   //   return axios.get('/api/bookshelf')
+//   // },
+
+//   getBookshelf: function (id) {
+//     return axios.get('/api/bookshelf')
+//   },
+
+//   deleteBook: function (id) {
+//     return axios.delete('/api/bookshelf/' +id);
+//   }
+// }
